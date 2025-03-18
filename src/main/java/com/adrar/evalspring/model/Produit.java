63cc1b9c940c7bd0fb1d76eaf281ba0ef33ca471
mithridatem <mathieumithridate@adrar-formation.com>
2025-03-18 22:17:38 +0100

    feat : correction partie n° 1

---
+++ src/main/java/com/adrar/evalspring/model/Produit.java
@@ -0,0 +1,56 @@
+package com.adrar.evalspring.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "produit")
+public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom", nullable = false, length = 50)
+    @NotBlank(message = "Le nom du produit ne doit pas être vide")
+    @Size(min = 2, max = 50, message = "Le nom du produit doit contenir entre 2 et 50 caractères")
+    private String nom;
+
+    @Column(name = "prix", nullable = false)
+    @NotBlank(message = "Le prix du produit ne doit pas être vide")
+    @Min(value = 0, message = "Le prix du produit doit être positif")
+    private Double prix;
+
+    public Produit() {}
+
+    public Produit(String nom, Double prix) {
+        this.nom = nom;
+        this.prix = prix;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+}
